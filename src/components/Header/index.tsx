102b6b838496021f6385c01b7447261f0eeb0ae1
Breno Façanha <brenofacanha.ti@gmail.com>
2025-05-08 19:15:09 -0300

    adicionando as seguintes bibliotecas phosphor e native svg e criado logica do botão voltar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,22 @@
-import { Image, View } from "react-native";
+import { Image, TouchableOpacity, View } from "react-native";
 import { styles } from "./style";
 import logoPng from "@assets/Logo.png"
+import { CaretLeft} from "phosphor-react-native"
 
-export function Header(){
+type Props = {
+    showBackButton?: boolean
+}
+
+export function Header({showBackButton = false}: Props){
     return(
         
-        <View>
-            <Image source={logoPng}></Image>
+        <View style={styles.container}>
+            {showBackButton && 
+                <TouchableOpacity style={styles.buton}>
+                    <CaretLeft color={styles.backIcon.color} size={32}/>
+                </TouchableOpacity>
+            }
+            <Image source={logoPng} width={46} height={55}></Image>
         </View>
         
     )
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,15 +1,22 @@
 import theme from "@theme/index";
-import { StyleSheet } from "react-native";
+import { Button, StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     container: {
         width: '100%' ,
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        marginTop: 50
     },
     logo: {
         width: 46,
-        height: 55
+        height: 55,
+    },
+    buton:{
+        flex: 1
+    },
+    backIcon: {
+        color: theme.COLORS.WHITE,
     }
 })
